a1d16076eedef05567576d4913b46ac1e1c4fe2c
Dicky <muhammad.dickynn@gmail.com>
2023-10-11 18:09:08 +0700

    Fetch Review Data From API

--- app/page.tsx
+++ app/page.tsx
@@ -49,6 +49,7 @@ const { Meta } = Card;
 
 interface reviewItems {
   id: number;
+  img: string;
   name: string;
   status: string;
   description: string;
@@ -61,24 +62,53 @@ interface reviewItems {
 // }
 
 const Home: React.FC = () => {
+
+  // const reviewItems = [
+  //   {
+  //     profile: <UserOutlined />,
+  //     name: 'Donald Sullivan',
+  //     status: 'Founder Fike',
+  //     description: 'This travel website is very informatife and easy to use. i like how they present various destination options and travel packages with clear details. Offerin pictures and destination descriptions helps me decide where i want to visit. Additionally, the ability to compare prices and reviews from other users is very helpful in decision making.'
+  //   },
+  //   {
+  //     profile: <UserOutlined />,
+  //     name: 'Muhammad Dicky',
+  //     status: 'Web Programmer',
+  //     description: 'jadi ceritanya ini adalah sebuah review dari sebuah web yang udah aku buat dengan data dummy tapi bentuknya sudah terformat dengan map sehingga ntar tinggal panggil aja data dari database'
+  //   }
+  // ]
+
   const [reviews, setReviews] = useState([]);
 
   const fetchReviews = async () => {
-
-    const reviewsResponse = await axiosInstance.get("/reviews")
-    setReviews(reviewsResponse.data)
-
+    try {
+      const reviewsResponse = await axiosInstance.get("/reviews")
+      setReviews(reviewsResponse.data)
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   const renderReviews = () => {
     return reviews.map((review: reviewItems) => {
       return (
-        <ul key={review.id.toString()}>
-          <li>{review.id}</li>
-          <li>{review.name}</li>
-          <li>{review.status}</li>
-          <li>{review.description}</li>
-        </ul>
+        <div key={review.id.toString()}>
+          <Row>
+            <Col span={2}></Col>
+            <Col span={20} style={{ margin: '0 auto' }}>
+              <Row>
+                <Avatar size="large" style={{ margin: '0 auto' }} icon={review.img} />
+                <div className='text-center' style={{ margin: '0 auto' }}>
+                  <b >{review.name}</b>
+                  <p>{review.status}</p>
+                  <p >{review.description}</p>
+                </div>
+              </Row>
+            </Col>
+            <Col span={2}></Col>
+          </Row>
+
+        </div>
       )
     })
   }
@@ -98,34 +128,34 @@ const Home: React.FC = () => {
           <MidCard />
           <CardImageTitleDestination />
           <CardOurDestination />
-          <ReviewItem />
+          {/* <ReviewItem /> */}
 
-          <div>
+          <Carousel autoplay className='mt-60 mb-60'>
             {renderReviews()}
-          </div>
+          </Carousel>
 
 
           {/* <Carousel autoplay className='mt-60 mb-60'>
-          {reviewItems.map((content, index) => (
-            <div key={index}>
-              <Row >
-                <Col span={2}></Col>
-                <Col span={20} style={{ margin: '0 auto' }}>
-                  <Row>
-                    <Avatar size="large" style={{ margin: '0 auto' }} icon={content.profile} />
-                    <div className='text-center' style={{ margin: '0 auto' }}>
-                      <b >{content.name}</b>
-                      <p>{content.status}</p>
-                      <p >{content.description}</p>
-                    </div>
-                  </Row>
-                </Col>
-                <Col span={2}></Col>
-              </Row>
-            </div>
-          ))}
+            {reviewItems.map((content, index) => (
+              <div key={index}>
+                <Row >
+                  <Col span={2}></Col>
+                  <Col span={20} style={{ margin: '0 auto' }}>
+                    <Row>
+                      <Avatar size="large" style={{ margin: '0 auto' }} icon={content.profile} />
+                      <div className='text-center' style={{ margin: '0 auto' }}>
+                        <b >{content.name}</b>
+                        <p>{content.status}</p>
+                        <p >{content.description}</p>
+                      </div>
+                    </Row>
+                  </Col>
+                  <Col span={2}></Col>
+                </Row>
+              </div>
+            ))}
 
-        </Carousel> */}
+          </Carousel> */}
 
 
 
